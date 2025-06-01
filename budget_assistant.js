aee00756f555734ed9f65291bbf7c3d269acbac9
Fernando Ramos <faramos@usf.edu>
2025-06-01 13:24:46 -0400

    initial-commit

---
+++ budget_assistant.js
@@ -0,0 +1,15 @@
+let startupName = "North South Charters";
+let initialCapital = 35000;
+let monthlyRevenue = 6000;
+let monthlyExpenses = 3500;
+
+// Step 2: Calculate Business Metrics
+let netCashFlow = monthlyRevenue - monthlyExpenses;
+let currentBalance = initialCapital + netCashFlow;
+let isProfitable = netCashFlow > 0;
+
+// Step 3: Print to Console
+console.log("Startup Name: " + startupName);
+console.log("Net Monthly Cash Flow: $" + netCashFlow);
+console.log("Current Balance: $" + currentBalance);
+console.log("Profitability Status: " + isProfitable);
